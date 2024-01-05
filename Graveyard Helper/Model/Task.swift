7ca63915e1d608946aa1eaa9af84980da762a65b
Pau Martinez <paumartinez@Paus-iMac-Pro.local>
2024-01-05 22:28:55 +0100

    NewCommit

---
+++ Graveyard Helper/Model/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  Graveyard Helper
+//
+//  Created by Pau Martinez on 4/1/24.
+//
+
+import Foundation
---
+++ Graveyard Helper/Views/Objects/RecipeItenView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeItenView.swift
+//  Graveyard Helper
+//
+//  Created by Pau Martinez on 4/1/24.
+//
+
+import SwiftUI
+
+struct RecipeItenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeItenView()
+}
